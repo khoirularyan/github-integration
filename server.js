2286e9bf4f3369fa706f4aa727ce5427b60aff0a
khoirularyan <khoirularyan123@gmail.com>
2025-10-15 08:26:53 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 9191;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+console.log("Starting the server...");
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
